74f390fca0fa1ceb79d44594348ac339d7d2cefb
EeroChiao <eero.chiao@udngroup.com.tw>
2024-12-04 14:47:35 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
